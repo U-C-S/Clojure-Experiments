d7de849728c3b05159602452d6c5fc14a43e2c84
U-C-S <uchanakyasrinivas@gmail.com>
2023-12-11 15:52:08 +0530

    init first-project

---
+++ first-project/src/first_project/core.clj
@@ -0,0 +1,12 @@
+(ns first-project.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(def name "John Doe")
+(str name " is " 25 " years old")
+
+(println name)
